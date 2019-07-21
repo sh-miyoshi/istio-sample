c654b26b669c82a010bf168d370d8384ee51e5d8
s.miyoshi <s.miyoshi1991@gmail.com>
2019-07-21 16:11:37 +0900

    add load generator script

---
+++ apps/bookinfo/loadgen.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# != 1 ]; then
+  echo "Usage: ./loadgen.sh <service-addr>"
+  exit 1
+fi
+
+SERVICE_ADDR=$1
+LOOP_NUM=100000
+SLEEP_TIME=2
+
+for i in `seq $LOOP_NUM`; do
+  status=`curl -o /dev/null -w '%{http_code}\n' -s $SERVICE_ADDR/productpage`
+  echo "status code: $status"
+
+  sleep $SLEEP_TIME
+done
